808ffc9341e97a08dc853e86c9d02331b7656fc2
LEE KYU WON <duffufk1202@naver.com>
2021-04-18 19:25:31 +0900

    Update spac prices in db

---
+++ src/update_prices.py
@@ -0,0 +1,52 @@
+import yfinance
+import pandas as pd
+import json
+import pymysql
+import sys
+
+db_config = json.loads(open('../env/db_config.json', 'r').read())
+connection = pymysql.connect(host=db_config['host'],
+                             port=db_config['port'],
+                             db=db_config['db'],
+                             user=db_config['user'],
+                             passwd=db_config['passwd'],
+                             autocommit=True,
+                             charset='utf8'
+                             )
+
+def insert_prices (ticker, ohlc_prices, conn):
+    for r in ohlc_prices.itertuples():
+        sql = f"REPLACE INTO prices values ('{ticker}', "\
+          f"'{r.Index}', '{r.Open}', '{r.High}', "\
+          f"'{r.Low}', '{r.Close}', '{r.Volume}')"
+
+        with conn.cursor() as curs:
+            curs.execute(sql)
+
+
+def fetch_price(ticker, start, end):
+    stock = yfinance.Ticker(ticker)
+    return stock.history(start=start, end=end)
+
+
+def get_spacs(from_ipo_date, to_ipo_date=None):
+    df = pd.read_csv('../data/spac_list.csv')
+
+    df = df[df['SPAC IPO Date'] >= from_ipo_date]
+    if(to_ipo_date):
+        df = df[df['SPAC IPO Date'] >= to_ipo_date]
+    return df
+
+
+def update_spac_prices(from_ipo_date):
+    spacs = get_spacs(from_ipo_date)
+
+    for r in spacs.itertuples():
+        print(f"Fetching {r[3]}")
+
+        ohlc_prices = fetch_price(r[2], r[15], r[6])
+        insert_prices(r[2], ohlc_prices, connection)
+
+if __name__ == '__main__':
+    from_date = sys.argv[1]
+    update_spac_prices(from_date)
