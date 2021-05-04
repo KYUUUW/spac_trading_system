1b601ec8a937b118cbf8097e0e4ef141710d1c6f
KYUUUW <duffufk1502@gmail.com>
2021-05-04 12:31:57 +0900

    check profit rate with spacs

--- src/CompanyImporter.py
+++ src/CompanyImporter.py
@@ -1,9 +1,14 @@
 import pandas as pd
+import json
 
 def import_all_company_tickers():
     list = pd.read_csv('../data/spac_list.csv')
     return list['Post-SPAC Ticker Symbol']
 
+def import_available_company():
+    with open('../data/available_companies.json', 'r') as f:
+        return json.loads(f.read())
+
 
 if __name__ == '__main__':
-    import_all_company_tickers()
+    print(import_available_company())
